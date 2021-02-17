e14612e02f445aa1554192e74d47dea6b886697d
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-02-17 18:29:47 +0100

    new task/added stars only for best cities - complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,7 @@ const getDataMapped = async () => {
         img: item.cover_image_url,
         name_city: item.name,
         content: item.content,
+        weight: item.weight,
         show_in_popular: item.show_in_popular,
       }
     });
@@ -40,12 +41,12 @@ const getDataMapped = async () => {
   return state.cities;
 }
 
-function createCards(country, imgUrl, city, description, popular) {
+function createCards(country, imgUrl, city, description, popular, weight) {
 
   const cardCity = document.createElement('div');           // contenitore intera card 
   const cardImg = document.createElement('img');            // contenitore immagine
   const cardDetails = document.createElement('div');        // contenitore descrizione 
-
+  const starWrapper = document.createElement('ul');
 
   const nameSection = document.createElement('h2');                    
   // const image = document.createElement('img');
@@ -61,16 +62,18 @@ function createCards(country, imgUrl, city, description, popular) {
   cardCity.classList.add('card__city');
   cardImg.classList.add('card__city--img');
   cardDetails.classList.add('card__city--description');
+  starWrapper.classList.add('star-wrapper', 'display-flex-row');
 
   // append all node
   // cardImg.appendChild(image);
-  cardDetails.append(nameCity, cardDescription);
+  cardDetails.append(nameCity, cardDescription, starWrapper);
   
   cardCity.append(cardImg, cardDetails);
 
   // countrySection.appendChild(nameSection);
 
   showInPopular(cardCity, popular);
+  showRating(weight, starWrapper);
 
   return cardCity;
 }
@@ -81,12 +84,48 @@ function showInPopular(card, popularCity){
   }
 }
 
+function showRating(weight, node){
+  let wholeStars = Math.floor(weight);
+  console.log(wholeStars); 
+
+  if (wholeStars >= 10) {
+    for (let i = 0; i < 4; i++) {
+      const newStar = document.createElement('i');
+      newStar.classList.add('fa', 'fa-star', 'star-color');
+      node.append(newStar);
+    }
+    for (let i = 0; i < 1; i++) {
+      const halfStar = document.createElement('i');
+      halfStar.classList.add('fa', 'fa-star', 'star_half-color');
+      node.append(halfStar);
+    }
+  }
+  
+
+  // switch(weight){
+  //   case weight >= 16:
+  //     console.log('16');
+  //     break;
+  //   case weight >= 12:
+  //     console.log('12');
+  //     break;  
+  //   case weight >= 8:
+  //     console.log('8');
+  //     break;  
+  //   case weight >= 4:
+  //     console.log('4');
+  //     break;    
+  //   default:
+  //     console.log("Errore"); 
+  // }
+}
+
 async function renderCards(country, list, node) {
   //filter per le country
   const result = await list.filter((data) => data.country === country)     //'Francia' || 'Italia' || 'Spagna' || 'Portogallo' || 'Grecia' || 'Germania'
   
   result.forEach((data) => {
-    const card = createCards(data.country, data.img, data.name_city, data.content, data.show_in_popular);
+    const card = createCards(data.country, data.img, data.name_city, data.content, data.show_in_popular, data.weight);
     
     node.appendChild(card);
   }); 
@@ -95,7 +134,7 @@ async function renderCards(country, list, node) {
 function runHtmlOver() {
   getDataMapped()
   .then(() => {
-      // console.log('new array mapped', state.cities)
+      console.log('new array mapped', state.cities)
       renderCards('Francia', state.cities, listFrancia);        
       renderCards('Italia', state.cities, listItalia);        
       renderCards('Spagna', state.cities, listSpagna);        
